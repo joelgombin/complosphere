b9d6c26986efef0efa9f7e20ce1fad335653f6c8
Joel Gombin <joel.gombin@gmail.com>
2014-07-30 00:30:50 +0200

    release juillet 2014

--- script.R
+++ script.R
@@ -8,85 +8,8 @@ reseau <- read.gexf("/media/Data/Dropbox/Thèse/counterpoints/complosphère/Hyph
 reseau2 <- gexf.to.igraph(reseau)
 E(reseau2)$weight <- reseau$edges$weight
 reseau <- reseau2
-# source("/media/Data/Dropbox/Thèse/counterpoints/complosphère/web/clique.R")
-# cliques <- largest.cliques(reseau)
-
-# communautés 
-# centralité 
-# connectivité
-# URL
-# nom 
-# couleur
-# thèmes
-
-
-
-# merger alterinfo.et et alterInfo.net 
-# supprimer aporrea.org
-# fusionner bruleparlesillumines.e-monsite.com et bruleparlesillumines.skyrock.com
-# supprimer buzz.yahoo.com
-# supprimer countercurrents.org
-# merger fr.ria.ru, fr.rian.ru et ria.ru
-# supprimer freetellafriend.com
-# merger globalresearch.ca et mondialisation.ca
-# supprimer independenza-webtv.com
-# merger info-palestine.eu et info-palestine.net
-# supprimer news.stcom.net
-# merger oulala.info et oulala.net
-# supprimer presstv.ir
-# fusionner r-sistons.over-blog.com et chantaldupille.over-blog.com
-# supprimer stcom.net
-# merger u-p-r.fr et u-p-r.org
-
-# supprimer acrimed.org
-# merger agoravox.fr et agoravox.tv
-# enlever bastamag.net
-# enlever christianophobie.fr
-# merger contre-info.com et contreinfo.info
-# deux fois diktacratie.com ??
-# enlever enquete-debat.fr ? 
-# enlever fdesouche.com
-# enlever metapedia.org
-# enlever novopress.info
-# merger fr.ria.ru et fr.rian.ru
-# enlever france-irak-actualite.com
-# enlever france-palestine.org
-# enlever horizons-et-debats.ch
-# merger info-palestine.net et info-palestine.eu (et enlever ?)
-# enlever initiative-communiste.fr ?
-# enlever ism-france.org
-# jre2014 figure deux fois
-# enlever la-dissidence.org
-# enlever lecontrarien.com
-# enlever lecourrierderussie.com
-# enlever lejournaldepersonne
-# enlever les-crises.fr
-# fusionner leschroniquesderorschach.blogspot.com et leschroniquesderorschach.blogspot.fr
-# enlever liesidotorg.wordpress.com
-# enlever medias-presse.info
-# michelcollon.info figure deux fois
-# mondialisation.ca figure deux fois
-# enlever nationspresse.info
-# enlever ndf.fr
-# newsoftomorrow.org figure deux fois
-# enlever ojim.fr
-# fusionner oulala.info et oulala.net
-# enlever oumma.com
-# enlever palestine-info.cc
-# enlever panamza.com
-# enlever piero.com
-# enlever polemia.com
-# enlever http://raimanet.wordpress.com/
-# fusionner u-p-r.fr et upr.fr
-# enlever ujfp.org
-# 
-
-
 reseau <- contract.vertices(reseau,
-#                  mapping = c(1:29,30,31:56,56,58:88,4,90:91,104,93:98,30,100:119,70,121:130),
-#                  mapping = match(c(1:29,30,31:56,56,58:88,4,90:91,104,93:98,30,100:119,70,121:130), unique(c(1:29,30,31:56,56,58:88,4,90:91,104,93:98,30,100:119,70,121:130))),
-#                  mapping = match(c(1:12,14:19,21:22,22,24:30,4,33:37,39:51,53, 55,53,57:59,62:69,62,71:81,83:85,88:93,95:96, 98:99,101:105, 107:109,111:113,85,85,117,109,101,125,128:130,133:134,57,136,138:140,91,142, 108, 144, 65, 150:154), unique(c(1:12,14:19,21:22,22,24:30,4,33:37,39:51,53, 55,53,57:59,62:69,62,71:81,83:85,88:93,95:96, 98:99,101:105, 107:109,111:113,85,85,117,109,101,125,128:130,133:134,57,136,138:140,91,142, 108, 144, 65, 150:154))),
-                  mapping = match(c(1:22,22,24:31,4,33:55,53,57:61, 70, 63:89,15,91:107,143,120,110:113,85,85,116:121,101,123,123,125:134,57,136:140,91,142:147,65,149:154), unique(c(1:22,22,24:31,4,33:55,53,57:61, 70, 63:89,15,91:107,143,120,110:113,85,85,116:121,101,123,123,125:134,57,136:140,91,142:147,65,149:154))),
+                  mapping = match(c(1:22,22,24:31,4,33:55,53,57:61, 70, 63:89,15,91:107,143,120,110:113,85,85,116:121,101,123,123,125:134,57,136:139,102,91,142:147,65,149:154), unique(c(1:22,22,24:31,4,33:55,53,57:61, 70, 63:89,15,91:107,143,120,110:113,85,85,116:121,101,123,123,125:134,57,136:139,102,91,142:147,65,149:154))),
                   vertex.attr.comb = list(name = "first"))
 V(reseau)$url <- gsub("Www ", "", V(reseau)$name)
 V(reseau)$url <- gsub(" ", ".", V(reseau)$url)
@@ -118,11 +41,6 @@ coords <- layout.kamada.kawai(reseau2, weights=E(reseau2)$weight)
 V(reseau2)$x <- coords[,1]
 V(reseau2)$y <- coords[,2]
 
-# on inverse les x 
-# V(reseau2)$x <- - (V(reseau2)$x - mean(V(reseau2)$x))
-
-
-# plot(reseau2)
 
 V(reseau2)$betweenness<- betweenness(reseau2)
 V(reseau2)$outcloseness <- closeness(reseau2, mode = "out")
@@ -156,8 +74,6 @@ couleurs <- as.data.frame(t(col2rgb(couleurs, alpha = TRUE)))
 couleurs$alpha <- 0.9
 names(couleurs) <- c("r", "g", "b", "alpha")
 
-# color.df <- data.frame(r = sapply(V(reseau2)$hclust, switch, "1" = 215, "2" = 255, "3" = 44), g = sapply(V(reseau2)$hclust, switch,  "1" = 25, "2" = 255, "3" = 123), b = sapply(V(reseau2)$hclust,switch,  "1" = 28, "2" = 191, "3" = 182), alpha = rep(0.9, length(V(reseau2))))
-
 write.gexf(nodes=data.frame(id = V(reseau2)$url, label=V(reseau2)$url),
            edges = data.frame(source = get.edgelist(reseau2)[,1], target = get.edgelist(reseau2)[,2]),
            edgesWeight = E(reseau2)$weight,
@@ -169,7 +85,7 @@ write.gexf(nodes=data.frame(id = V(reseau2)$url, label=V(reseau2)$url),
                               size=as.numeric(V(reseau2)$alexa),
                               color=couleurs),
            output="reseauJuillet2014.gexf",
-           defaultedgetype="mutual")
+           defaultedgetype="directed")
            
 
 
